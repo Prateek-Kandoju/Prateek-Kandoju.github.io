92e86e541bc5b61dc5653634d057f9387847f44a
Prateek-Kandoju <prateekkandoju@gmail.com>
2025-05-17 18:48:20 +0530

    up

--- a13/n.js
+++ a13/n.js
@@ -1,10 +1,12 @@
 let fruits = [
-  { id: 1, name: "Apple", price: 250 },
-  { id: 2, name: "Orange", price: 100 },
-  { id: 3, name: "Mango", price: 80 },
+  { id: 1, name: "Apple", price: 250 , status : "pending" },
+  { id: 2, name: "Orange", price: 100, status : "pending"  },
+  { id: 3, name: "Mango", price: 80 , status : "pending" },
 ];
 
-fruits.forEach((element) => {
-    element.price+=5;
-  console.log(element.name+" "+element.price);
-});
+let updatedFruits = fruits.map(element => ({...element , price : element.price+5 , status : "completed"}));
+ 
+
+updatedFruits.forEach(element => {
+  console.log(element.price+" "+element.status);
+})
