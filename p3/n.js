79b2b686a44058fbc0bac2f7a67042cfb209c55f
Prateek-Kandoju <prateekkandoju@gmail.com>
2025-05-18 19:50:48 +0530

    up

--- p3/n.js
+++ p3/n.js
@@ -1,29 +1,29 @@
 const products = [
-  { id: 1, name: "p1", price: 34 },
-  { id: 2, name: "p2", price: 50 },
-  { id: 3, name: "p3", price: 75 },
-];
+      { id: 1, name: "p1", price: 34 },
+      { id: 2, name: "p2", price: 50 },
+      { id: 3, name: "p3", price: 75 },
+    ];
 
-const cart = {};
+    const cart = {};
 
-const showProducts = () => {
-  const root = document.getElementById("root");
-  let html = "";
+    const showProducts = () => {
+      const root = document.getElementById("root");
+      let html = "";
 
-  for (let i = 0; i < products.length; i++) {
-    const product = products[i];
-    html +=
-      "<div class='product-box'>" +
-      "<h3>" + product.name + "</h3>" +
-      "<p>Price: $" + product.price + "</p>" +
-      "<button onclick='addToCart(" + product.id + ")'>Add to Cart</button>" +
-      "</div>";
-  }
+      for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        html +=
+          "<div class='product-box'>" +
+          "<h3>" + product.name + "</h3>" +
+          "<p>Price: ₹" + product.price + "</p>" +
+          "<button onclick='addToCart(" + product.id + ")'>Add to cart</button>" +
+          "</div>";
+      }
 
-  root.innerHTML = html;
-};
+      root.innerHTML = html;
+    };
 
-const addToCart = (id) => {
-  cart[id] = (cart[id] || 0) + 1;
-  alert("Product " + id + " added to cart. Qty: " + cart[id]);
-};
+    const addToCart = (id) => {
+      cart[id] = (cart[id] || 0) + 1;
+      alert("Product " + id + " added to cart. Quantity: " + cart[id]);
+    };
